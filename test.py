c11af90a80e1bde68f7e07a39d68ea675ee2897e
Małgorzata Leszczuk <gosialeszczuk@gmail.com>
2020-09-05 18:50:48 +0200

    dodanie funkcji goToWebpage

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 from selenium import webdriver
-driver = webdriver.Chrome(executable_path=r"C:\Users\Acer\Desktop\Programowanko\sele_python\chromedriver\driver\chromedriver.exe")
-driver.get("http://www.google.com")
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+driver = webdriver.Chrome(
+        executable_path=r"C:\Users\Acer\Desktop\Programowanko\sele_python\chromedriver\driver\chromedriver.exe")
+
+
+# go to webpage
+def gotoWebpage(website):
+    driver.get(website)
+
+webpage="http://automationpractice.com/index.php"
+gotoWebpage(webpage)
+
