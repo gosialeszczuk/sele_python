9743eba2954d169352c726f727b5fc539b7d3ed8
Małgorzata Leszczuk <gosialeszczuk@gmail.com>
2020-09-05 17:50:25 +0200

    dodanie selenium do projektu

---
+++ test.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path=r"C:\Users\Acer\Desktop\Programowanko\sele_python\chromedriver\driver\chromedriver.exe")
+driver.get("http://www.google.com")
