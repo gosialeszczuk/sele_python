3b95a1aa0aaded464017ddc8a986049837b9f008
Małgorzata Leszczuk <gosialeszczuk@gmail.com>
2020-09-06 20:07:23 +0200

    dodanie newslettera i funkcji signin

--- test.py
+++ test.py
@@ -1,21 +1,35 @@
 from selenium import webdriver
-
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome(
     executable_path=r"C:\Users\Acer\Desktop\Programowanko\sele_python\chromedriver\driver\chromedriver.exe")
 
 
-# go to webpage
+
 def gotoWebpage():
+    driver.maximize_window()
     driver.get("http://automationpractice.com/index.php")
 
-def newsletter():
+
+def newsletter(email):
     gotoWebpage()
-    element=driver.find_element_by_id("newsletter-input")
-    element.send_keys("test@test.com")
-   # element.find_element_by_xpath("//*[@id="newsletter_block_left"]/div/form/div/button")
-    # element.find_element_by_class_name("alert alert-success")
+    element = driver.find_element_by_id("newsletter-input")
+    element.send_keys(email)
+    element = driver.find_element_by_xpath("/html/body/div/div[3]/footer/div/div[1]/div/form/div/button")
+    action = ActionChains(driver)
+    action.click(on_element=element)
+    action.perform()
+    element.click()
 
 
-newsletter()
+def sigin():
+    gotoWebpage()
+    element = driver.find_element_by_xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[1]/a")
+    element.click()
+
+
+
+
+
+newsletter("test@test.pl")
+
